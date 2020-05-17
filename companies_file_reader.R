34ed039ed8c7519eaaeac56ca835389a80b6e0a7
ioanna-toufexi <ioanna.toufexi@city.ac.uk>
2020-05-17 22:36:46 +0100

    basic work for both streaming api and csv

---
+++ companies_file_reader.R
@@ -0,0 +1,42 @@
+if(!require("pacman")) install.packages("pacman")
+pacman::p_load(readr, dplyr, lubridate, zoo, ggplot2, stringr,
+               purrr, tidyr)
+
+extract_SIC <- function(text) {
+  str_extract(text, "^[0-9]+")
+}
+
+get_companies_from_file <- function(csv_path) {
+  
+  all_companies <- read_csv(csv_path)
+  
+  selected <- all_companies %>% 
+    select(CompanyName, 
+           CompanyNumber, 
+           RegAddress.AddressLine1,
+           RegAddress.AddressLine2,
+           RegAddress.PostTown, 
+           RegAddress.PostCode,
+           CompanyCategory,
+           CompanyStatus,
+           DissolutionDate,
+           IncorporationDate,
+           Accounts.AccountCategory,
+           #CountryOfOrigin,
+           SIC1 = SICCode.SicText_1,
+           SIC2 = SICCode.SicText_2,
+           SIC3 = SICCode.SicText_3,
+           SIC4 = SICCode.SicText_4) %>% 
+    mutate(SIC1 = extract_SIC(SIC1),
+           SIC2 = extract_SIC(SIC2), 
+           SIC3 = extract_SIC(SIC3),
+           SIC4 = extract_SIC(SIC4)) %>% 
+    unite(SICCode, c(SIC1, 
+                     SIC2,
+                     SIC3, 
+                     SIC4), sep = " ", remove=FALSE)
+  
+}
+
+get_companies_from_file("C:/Users/ioanna/Downloads/BasicCompanyDataAsOneFile-2020-05-01/BasicCompanyDataAsOneFile-2020-05-01.csv")
+
--- stream.R
+++ stream.R
@@ -1,12 +1,12 @@
 if(!require("pacman")) install.packages("pacman")
-pacman::p_load(httr, readr, stringr, jsonlite)
+pacman::p_load(httr, readr, stringr, jsonlite, dplyr)
 
 url <- "https://stream.companieshouse.gov.uk/"
 
 getCompanies <- function(file.name=NULL, timeout=0, timepoint)
 {
-  #file.name <- "test.json"
-  #timeout <- 1
+  #file.name <- "data/test18.json"
+  #timeout <- 10
   #timepoint <- 9982490
   con <- file(description=file.name, open="a")
   
@@ -31,15 +31,15 @@ getCompanies <- function(file.name=NULL, timeout=0, timepoint)
   
   if (!is.null(file.name)){ close(con) }
   
-  #file.name <- "broadcast.json"
+  #file.name <- "data/broadcast.json"
   
   s <- read_file(file.name)
   
   s <- str_c("[",s,"]",collapse = "")
   
   s <- s %>% 
-    str_remove_all(s, "\n") %>% 
-    str_remove_all(s, "\r")
+    str_remove_all("\n") %>% 
+    str_remove_all("\r")
 
   #s <- gsub("}}\r\n{\"resource_kind", "}},\r\n{\"resource_kind", s, fixed= T)
   s <- gsub("}}{\"resource_kind", "}},{\"resource_kind", s, fixed= T)
@@ -47,6 +47,27 @@ getCompanies <- function(file.name=NULL, timeout=0, timepoint)
   #write_file(s, "applied.json")
   
   streamed_df <- fromJSON(s, flatten = TRUE)
+  
+   selected_df <- streamed_df %>% 
+     filter(resource_kind == "company-profile") %>% 
+     select(CompanyNumber = data.company_number,
+            CompanyName = data.company_name,
+            RegAddress.AddressLine1 = data.registered_office_address.address_line_1,
+            RegAddress.AddressLine2 = data.registered_office_address.address_line_2,
+            RegAddress.PostTown = data.registered_office_address.locality,
+            RegAddress.PostCode = data.registered_office_address.postal_code,
+            CompanyCategory = data.type,
+            CompanyStatus = data.company_status,
+            Accounts.AccountCategory = data.accounts.last_accounts.type,
+            DissolutionDate = data.date_of_cessation,
+            IncorporationDate = data.date_of_creation,
+            SICCode.SicText_1 = data.sic_codes
+       
+            )
+  streamed_df
 }
 
-getCompanies("data/test14.json", 1, 9982490)
+#getCompanies("data/test14.json", 1, 9982490)
+
+streamed_df <- getCompanies("data/test20.json", 300, 9750000)
+
