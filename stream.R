96b5223dbeff22526d9138a38e848807ef0bf670
ioanna-toufexi <ioanna.toufexi@city.ac.uk>
2020-05-24 12:00:13 +0100

    stream.R improvements

--- stream.R
+++ stream.R
@@ -3,11 +3,11 @@ pacman::p_load(httr, readr, stringr, jsonlite, dplyr)
 
 url <- "https://stream.companieshouse.gov.uk/"
 
-getCompanies <- function(file.name=NULL, timeout=0, timepoint)
+getCompanies <- function(file.name=NULL, timeout=60, timepoint=NULL)
 {
-  file.name <- "data/test130.json"
-  timeout <- 100
-  timepoint <- 9692100
+  if (is.null(file.name)) {
+    file.name <- str_c("data/stream_", format(Sys.time(), "%Y-%m-%d_%H%M%S_%Z"), ".json")
+  }
   con <- file(description=file.name, open="a")
   
   write_fun <- function(con) {
@@ -19,19 +19,17 @@ getCompanies <- function(file.name=NULL, timeout=0, timepoint)
   r <- tryCatch(GET(
     url = url,
     path = "companies",
-    config(timeout = 3),
+    config(timeout = timeout),
     query = list(timepoint = timepoint),
     write_stream(write_fun(con)),
     #add_headers(timeout = 300),
-    add_headers(Authorization = "E_QO_pPF4kGUGJ4yFoD2v75eLBvn_HOsPd3cIOk_"),
+    add_headers(Authorization = Sys.getenv('CH_STR_API_AUTH')),
     add_headers(Accept = "application/json"),
     add_headers(`Accept-Encoding` = "gzip, deflate")),
     error = function(e) return(e))
   
   
   if (!is.null(file.name)){ close(con) }
-  
-  #file.name <- "data/test121.json"
   
   s <- read_file(file.name)
   
@@ -41,10 +39,7 @@ getCompanies <- function(file.name=NULL, timeout=0, timepoint)
     str_remove_all("\n") %>% 
     str_remove_all("\r")
 
-  #s <- gsub("}}\r\n{\"resource_kind", "}},\r\n{\"resource_kind", s, fixed= T)
   s <- gsub("}}{\"resource_kind", "}},{\"resource_kind", s, fixed= T)
-  
-  #write_file(s, "applied.json")
   
   streamed_df <- fromJSON(s, flatten = TRUE)
   
@@ -59,16 +54,16 @@ getCompanies <- function(file.name=NULL, timeout=0, timepoint)
             CompanyCategory = data.type,
             CompanyStatus = data.company_status,
             Accounts.AccountCategory = data.accounts.last_accounts.type,
-            DissolutionDate = data.date_of_cessation,
+            #DissolutionDate = data.date_of_cessation,
             IncorporationDate = data.date_of_creation,
             SICCode = data.sic_codes) %>% 
      mutate(SICCode = str_c(SICCode, collapse = " "))
    
    
-  streamed_df
+  selected_df
 }
 
 #getCompanies("data/test14.json", 1, 9982490)
 
-streamed_df <- getCompanies("data/test25.json", 1, 9700000)
+companies <- getCompanies()
 
