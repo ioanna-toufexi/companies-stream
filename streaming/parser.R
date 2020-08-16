10d7ecc52172e4967ea77e25bbfe9eecf40ffc16
ioanna-toufexi <ioanna.toufexi@city.ac.uk>
2020-08-16 10:55:07 +0100

    WIP

--- streaming/parser.R
+++ streaming/parser.R
@@ -1,5 +1,11 @@
+get_stream_data_from_file <- function(file.name) {
+  
+  streamed_df <- parse_stream_from_file(file.name)
+  filter_stream(streamed_df)
+}
 
-format_stream_data <- function(file.name) {
+
+parse_stream_from_file <- function(file.name) {
   s <- read_file(file.name)
   
   s <- str_c("[",s,"]",collapse = "")
@@ -10,23 +16,30 @@ format_stream_data <- function(file.name) {
   
   s <- gsub("}}{\"resource_kind", "}},{\"resource_kind", s, fixed= T)
   
-  streamed_df <- fromJSON(s, flatten = TRUE)
-  
+  fromJSON(s, flatten = TRUE)
+}
+
+filter_stream <- function(streamed_df) {
+
   selected_df <- streamed_df %>% 
-    filter(resource_kind == "company-profile") %>% 
-    select(CompanyNumber = data.company_number,
-           CompanyName = data.company_name,
-           RegAddress.AddressLine1 = data.registered_office_address.address_line_1,
-           RegAddress.AddressLine2 = data.registered_office_address.address_line_2,
-           RegAddress.PostTown = data.registered_office_address.locality,
-           RegAddress.PostCode = data.registered_office_address.postal_code,
-           CompanyCategory = data.type,
-           CompanyStatus = data.company_status,
-           Accounts.AccountCategory = data.accounts.last_accounts.type,
-           #DissolutionDate = data.date_of_cessation,
-           IncorporationDate = data.date_of_creation,
-           SICCode = data.sic_codes)# %>% 
-  #mutate(SICCode = str_c(SICCode, collapse = " "))
+    filter(resource_kind == "company-profile")
+  
+  # %>% 
+  #   select(CompanyNumber = data.company_number,
+  #          CompanyName = data.company_name,
+  #          RegAddress.AddressLine1 = data.registered_office_address.address_line_1,
+  #          RegAddress.AddressLine2 = data.registered_office_address.address_line_2,
+  #          RegAddress.PostTown = data.registered_office_address.locality,
+  #          RegAddress.PostCode = data.registered_office_address.postal_code,
+  #          CompanyCategory = data.type,
+  #          CompanyStatus = data.company_status,
+  #          Accounts.AccountCategory = data.accounts.last_accounts.type,
+  #          DissolutionDate = data.date_of_cessation,
+  #          IncorporationDate = data.date_of_creation,
+  #          EventTimepoint = event.timepoint,
+  #          SICCode = data.sic_codes)# %>% 
+  # #mutate(SICCode = str_c(SICCode, collapse = " "))
+  
   
   
   selected_df
--- streaming/streaming_script.R
+++ streaming/streaming_script.R
@@ -1,6 +1,35 @@
-source("api_connector.R")
-source("parser.R")
+if(!require("pacman")) install.packages("pacman")
+pacman::p_load(lubridate)
 
-file_name <- storeCompaniesFromStream(timeout_in_secs = 60, timepoint = "16616286")
+source("streaming/streamer.R")
+source("streaming/parser.R")
 
-selected_df <- format_stream_data(file_name)
+file_name <- storeCompaniesFromStream(timeout_in_secs = 600, timepoint = "16998847")
+
+selected_df <- get_stream_data_from_file("data/16441500_to_16499999.json")
+
+selected_df1 <- get_stream_data_from_file("data/16500000_to_16998846_stream_2020-08-12_221533_BST.json")
+
+selected_df2 <- get_stream_data_from_file("data/16998847_to_17055978_stream_2020-08-14_203615_BST.json")
+
+the_data <- bind_rows(selected_df,selected_df1,selected_df2)
+
+filtered_df <- the_data %>% 
+  mutate(DissolutionDate = ymd(data.date_of_cessation)) %>% 
+  filter(DissolutionDate > ymd("2020-07-31")) %>% 
+  filter(data.company_status != "active")
+
+filtered_df1 <- the_data %>% 
+  mutate(DissolutionDate = ymd(data.date_of_cessation)) %>% 
+  filter(DissolutionDate > ymd("2020-07-31")) %>% 
+  filter(data.company_status == "active")
+
+filtered_df2 <- the_data %>% 
+  mutate(CreationDate = ymd(data.date_of_creation)) %>% 
+  filter(CreationDate > ymd("2020-07-31")) %>% 
+  filter(data.company_status == "active")
+
+filtered_df3 <- the_data %>% 
+  mutate(CreationDate = ymd(data.date_of_creation)) %>% 
+  filter(CreationDate > ymd("2020-07-31")) %>% 
+  filter(data.company_status != "active")
