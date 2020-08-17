0b21b08540871e6816a0a5226bcc406590c16238
ioanna-toufexi <ioanna.toufexi@city.ac.uk>
2020-08-17 11:44:21 +0100

    removing duplicates from stream - wip

--- streaming/parser.R
+++ streaming/parser.R
@@ -22,23 +22,21 @@ parse_stream_from_file <- function(file.name) {
 filter_stream <- function(streamed_df) {
 
   selected_df <- streamed_df %>% 
-    filter(resource_kind == "company-profile")
-  
-  # %>% 
-  #   select(CompanyNumber = data.company_number,
-  #          CompanyName = data.company_name,
-  #          RegAddress.AddressLine1 = data.registered_office_address.address_line_1,
-  #          RegAddress.AddressLine2 = data.registered_office_address.address_line_2,
-  #          RegAddress.PostTown = data.registered_office_address.locality,
-  #          RegAddress.PostCode = data.registered_office_address.postal_code,
-  #          CompanyCategory = data.type,
-  #          CompanyStatus = data.company_status,
-  #          Accounts.AccountCategory = data.accounts.last_accounts.type,
-  #          DissolutionDate = data.date_of_cessation,
-  #          IncorporationDate = data.date_of_creation,
-  #          EventTimepoint = event.timepoint,
-  #          SICCode = data.sic_codes)# %>% 
-  # #mutate(SICCode = str_c(SICCode, collapse = " "))
+    filter(resource_kind == "company-profile") %>% 
+    select(CompanyNumber = data.company_number,
+           CompanyName = data.company_name,
+           RegAddress.AddressLine1 = data.registered_office_address.address_line_1,
+           RegAddress.AddressLine2 = data.registered_office_address.address_line_2,
+           RegAddress.PostTown = data.registered_office_address.locality,
+           RegAddress.PostCode = data.registered_office_address.postal_code,
+           CompanyCategory = data.type,
+           CompanyStatus = data.company_status,
+           Accounts.AccountCategory = data.accounts.last_accounts.type,
+           DissolutionDate = data.date_of_cessation,
+           IncorporationDate = data.date_of_creation,
+           EventTimepoint = event.timepoint,
+           SICCode = data.sic_codes) %>%
+  mutate(SICCode = str_c(SICCode, collapse = " "))
   
   
   
--- streaming/streaming_script.R
+++ streaming/streaming_script.R
@@ -6,30 +6,16 @@ source("streaming/parser.R")
 
 file_name <- storeCompaniesFromStream(timeout_in_secs = 600, timepoint = "16998847")
 
-selected_df <- get_stream_data_from_file("data/16441500_to_16499999.json")
+stream_data_df <- get_stream_data_from_file("data/16998847_to_17055978_stream_2020-08-14_203615_BST.json")
 
-selected_df1 <- get_stream_data_from_file("data/16500000_to_16998846_stream_2020-08-12_221533_BST.json")
+#the_data <- bind_rows(df,df1,df2)
 
-selected_df2 <- get_stream_data_from_file("data/16998847_to_17055978_stream_2020-08-14_203615_BST.json")
-
-the_data <- bind_rows(selected_df,selected_df1,selected_df2)
-
-filtered_df <- the_data %>% 
-  mutate(DissolutionDate = ymd(data.date_of_cessation)) %>% 
-  filter(DissolutionDate > ymd("2020-07-31")) %>% 
-  filter(data.company_status != "active")
-
-filtered_df1 <- the_data %>% 
-  mutate(DissolutionDate = ymd(data.date_of_cessation)) %>% 
-  filter(DissolutionDate > ymd("2020-07-31")) %>% 
-  filter(data.company_status == "active")
-
-filtered_df2 <- the_data %>% 
+df <- stream_data_df %>% 
   mutate(CreationDate = ymd(data.date_of_creation)) %>% 
-  filter(CreationDate > ymd("2020-07-31")) %>% 
-  filter(data.company_status == "active")
+  filter(CreationDate > ymd("2020-07-31"))# %>% 
+  #filter(data.company_status == "active")
 
-filtered_df3 <- the_data %>% 
-  mutate(CreationDate = ymd(data.date_of_creation)) %>% 
-  filter(CreationDate > ymd("2020-07-31")) %>% 
-  filter(data.company_status != "active")
+df <- df[order(df$data.company_number, -order(df$event.timepoint) ), ]
+unique_rows_df <- df[ !duplicated(df$data.company_number), ]
+
+any <- unique_rows_df %>% filter(data.company_status != "active")
