d802dda00ed0626d63275b3c3e9ffc5e1cafc25a
ioanna-toufexi <ioanna.toufexi@city.ac.uk>
2020-08-17 20:25:16 +0100

    sorting out the SIC codes

--- streaming/parser.R
+++ streaming/parser.R
@@ -1,3 +1,5 @@
+library(purrr)
+
 get_stream_data_from_file <- function(file.name) {
   
   streamed_df <- parse_stream_from_file(file.name)
@@ -23,22 +25,36 @@ filter_stream <- function(streamed_df) {
 
   selected_df <- streamed_df %>% 
     filter(resource_kind == "company-profile") %>% 
-    select(CompanyNumber = data.company_number,
-           CompanyName = data.company_name,
+    select(CompanyName = data.company_name,
+           CompanyNumber = data.company_number,
            RegAddress.AddressLine1 = data.registered_office_address.address_line_1,
            RegAddress.AddressLine2 = data.registered_office_address.address_line_2,
            RegAddress.PostTown = data.registered_office_address.locality,
            RegAddress.PostCode = data.registered_office_address.postal_code,
            CompanyCategory = data.type,
            CompanyStatus = data.company_status,
-           Accounts.AccountCategory = data.accounts.last_accounts.type,
-           DissolutionDate = data.date_of_cessation,
            IncorporationDate = data.date_of_creation,
-           EventTimepoint = event.timepoint,
-           SICCode = data.sic_codes) %>%
-  mutate(SICCode = str_c(SICCode, collapse = " "))
+           Accounts.AccountCategory = data.accounts.last_accounts.type,
+           SICCode = data.sic_codes)
   
+  selected_df <- selected_df %>% 
+    mutate(SICCode = map(SICCode,conc)) %>% 
+    run_on_df(unlist)
   
   
   selected_df
+}
+
+
+# Runs a given function on all columns of a dataframe
+run_on_df <- function(df,func) {
+  for (i in seq_along(df)) {
+    df[[i]] <- func(df[[i]])
+  }
+  df
+}
+
+# Concatenates vector data
+conc <- function(x) {
+    str_c(x,collapse=" ")
 }
